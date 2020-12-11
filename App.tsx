4547a80ee949051b08a6a7c324dc5aa1c3d58d38
Ryan-Solomon <ryanjsolomon24@gmail.com>
2020-12-10 18:17:59 -0600

    init commit

--- App.tsx
+++ App.tsx
@@ -1,21 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './src/pages/HomeScreen';
 
-export default function App() {
+type RootStackParamList = {
+  Home: undefined;
+};
+
+const RootStack = createStackNavigator<RootStackParamList>();
+
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <RootStack.Navigator initialRouteName='Home'>
+      <RootStack.Screen name='Home' component={HomeScreen} />
+    </RootStack.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
